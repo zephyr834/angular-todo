790347dd25c0322dd17326e0c1c02ab0ba36e63a
Cory Eighan <zephyr834@gmail.com>
2019-05-31 17:32:43 -0700

    Styled components and added button func to checkbox and delete.

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -13,4 +13,21 @@ export class TodoItemComponent implements OnInit {
   ngOnInit() {
   }
 
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
+
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+  todos:Todo[];
+  
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo one',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo 2',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Todo 3',
+        completed: false
+      }
+    ]
   }
 
 }
