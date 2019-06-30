7d874ec5313b2386ed90998f80a5078f984b9695
Cory Eighan <zephyr834@gmail.com>
2019-06-29 17:19:57 -0700

    Added todo service. Changed it to use REST API and get todo list from placeholder.

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import{ TodoService } from '../../services/todo.service'
 
 @Component({
   selector: 'app-todos',
@@ -9,26 +10,12 @@ import { Todo } from 'src/app/models/Todo';
 export class TodosComponent implements OnInit {
   todos:Todo[];
   
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo one',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo 2',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Todo 3',
-        completed: false
-      }
-    ]
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
   }
 
 }
